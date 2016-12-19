351b6f4024379565662ce8a2f35ec7e9dbb3ffec
Abe <abe.torres@utexas.edu>
2016-12-18 18:47:21 -0600

    ...

--- Android Application/Ellomix/app/src/main/java/com/ellomix/android/ellomix/ProfileFragment.java
+++ Android Application/Ellomix/app/src/main/java/com/ellomix/android/ellomix/ProfileFragment.java
@@ -22,7 +22,7 @@ public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.test_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
 
         return v;
     }
