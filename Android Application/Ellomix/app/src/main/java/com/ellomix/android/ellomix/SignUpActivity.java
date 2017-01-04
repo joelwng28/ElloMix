8f3c3c1e9866de4c57f3ab81a92e26da75b0d669
Emcarrasco <elenamcarrasco@gmail.com>
2017-01-03 18:19:29 -0800

    updated UI general flow -Elena C

--- Android Application/Ellomix/app/src/main/java/com/ellomix/android/ellomix/SignUpActivity.java
+++ Android Application/Ellomix/app/src/main/java/com/ellomix/android/ellomix/SignUpActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private Button mySignUpButton;
+    private ImageButton myAddPhotoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,7 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         mySignUpButton = (Button) findViewById(R.id.appSignUpButton);
+        myAddPhotoButton = (ImageButton) findViewById(R.id.AddPhotoButton);
 
         mySignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,10 +26,24 @@ public class SignUpActivity extends AppCompatActivity {
                 goConnectSocialMediaScreen();
             }
         });
+
+        myAddPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goAddPhotoScreen();
+            }
+        });
+
     }
 
     protected void goConnectSocialMediaScreen() {
         Intent i = new Intent(this, ConnectSocialMediaActivity.class);
+        startActivity(i);
+    }
+
+
+    protected void goAddPhotoScreen() {
+        Intent i = new Intent(this, AddPhotoActivity.class);
         startActivity(i);
     }
 }
